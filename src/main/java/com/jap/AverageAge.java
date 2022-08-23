e76f4a2e4639736ae6d445399a141d83dd478546
Rohit Raj <rohit2000lmp@gmail.com>
2022-08-23 18:23:31 +0530

    updated

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -5,10 +5,15 @@ public class AverageAge {
     public String averageAgeCalculator(String[] ageRawData)
     {
         double avg = 0;
-
+        float sum = 0;
+        int i = 0;
         try{
             //Write the logic
-
+            while (i<ageRawData.length){
+                sum += Integer.parseInt(ageRawData[i]);
+                i++;
+            }
+            avg = sum/ageRawData.length;
             //Handle specific exception
         }catch (Exception exception)
         {
